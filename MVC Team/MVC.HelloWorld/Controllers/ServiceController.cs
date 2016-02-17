72017562febb18ca2ebda9a2fae6495eade4b3d1
armanav <armanavetisyan52@gmail.com>
2016-02-17 21:15:43 +0400

    Create Service controller and edit it

---
+++ MVC Team/MVC.HelloWorld/Controllers/ServiceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.HelloWorld.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Pizza()
+        { return View(); }
+    }
+}
---
+++ MVC Team/MVC.HelloWorld/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.HelloWorld.Startup))]
+namespace MVC.HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
