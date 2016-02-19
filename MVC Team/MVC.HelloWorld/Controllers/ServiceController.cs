5a9c8010b2f28a4a2a7dfe3fc6c6773384dd3dc6
armanveranyan <arman.v18@gmail.com>
2016-02-19 20:58:06 +0400

    Nare

--- MVC Team/MVC.HelloWorld/Controllers/ServiceController.cs
+++ MVC Team/MVC.HelloWorld/Controllers/ServiceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC.HelloWorld.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,7 +14,21 @@ namespace MVC.HelloWorld.Controllers
         {
             return View();
         }
-        public ActionResult Pizza()
-        { return View(); }
+        public ActionResult Pizza(int page=1)
+        {
+            ViewBag.page = page;
+            ViewData["page"] = page;
+            for (int i = 0; i < 10; i++)
+            {
+                ViewData[$"data{i}"] = i;
+            }
+            var count = 10;
+            var list = ModelFactory.GetPizzaList();
+            var model = list
+                .Skip((page - 1) * count)
+                .Take(count)
+                .Select(p => ModelFactory.Create(p));
+            return View(model);
+        }
     }
 }
---
+++ MVC Team/MVC.HelloWorld/Models/ModelFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.HelloWorld.Models
+{
+    class ModelFactory
+    {
+        public static PizzaViewModel Create (Pizza pizza)
+        {
+            return new PizzaViewModel { Name = pizza.Name, Price = pizza.Price };
+        }
+        public static List<Pizza> GetPizzaList()
+        {
+            var list = new List<Pizza>()
+            {
+                new Pizza
+                {
+                ID =Guid.NewGuid().ToString(),
+                Name="Peperoni",
+                Price =3000
+                },
+                 new Pizza
+                {
+                ID =Guid.NewGuid().ToString(),
+                Name="Tashir",
+                Price =4500
+                },
+            };
+            return list;
+        }
+    }
+}
