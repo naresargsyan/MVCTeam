63e2b998455d0ba58df42dc246b83098a644816d
armanveranyan <arman.v18@gmail.com>
2016-02-19 21:01:06 +0400

    Nare

---
+++ MVC Team/MVC.HelloWorld/Models/ServiceViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.HelloWorld.Models
+{
+    public class Pizza
+    {
+       public string ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+    public class PizzaViewModel
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+        
+    
+}
