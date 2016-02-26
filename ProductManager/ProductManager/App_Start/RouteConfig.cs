356ba64e7ad74d5d71b6870921e32c803756fa2a
Nare <nare.sargsian@gmail.com>
2016-02-26 21:14:26 +0400

    sfsdfsd

--- ProductManager/ProductManager/App_Start/RouteConfig.cs
+++ ProductManager/ProductManager/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace ProductManager
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            //routes.MapRoute(
+            //    name: "Search",
+            //    url: "{Controller}?q=term",
+            //     defaults: new {term= UrlParameter.Optional },
+            //     )
         }
     }
 }
---
+++ ProductManager/ProductManager/Controllers/SearchController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManager.Controllers
+{
+    public class SearchController : Controller
+    {
+        private ProductManagerDbContext db = new ProductManagerDbContext();
+        // GET: Search
+        public async Task<ActionResult> Index(string ID)
+        {
+            var products = await db.Products.Where(p => p.Name.Contains(ID)).ToListAsync();
+            return View(products);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
